3d316bbeec206e3e045678d25e149f0071ba6f97
“amandajene08” <amandajene08@gmail.com>
2022-03-07 08:58:02 -0500

    JSBasics

---
+++ script.js
@@ -0,0 +1,56 @@
+//  1
+console.log(21);
+//  2 
+// Numbers, Strings, Boolean, Null, Undefined.
+
+// 3
+/* Modulus is %, 
+Exponentiation is **, 
+Multiplication is *, 
+Division is /, 
+Addition is +, 
+Subtraction is -
+*/
+
+// 4
+console.log(5 % 2);  // 1
+console.log(5 % 4);  // 1
+
+// 5
+console.log('"I\'m Mary Poppins, y\'all!"');  // "I'm Mary Poppins, y'all!"
+
+// 6
+console.log("Hawk" + "eye");  // HawkEye
+
+// 7
+console.log("Amanda".length + "Spradlin".length);  // 13
+
+// 8
+console.log("Avengers: Endgame"[16]);  // e
+
+// BONUS
+// 1
+// First backslash to allow special characters to show.
+console.log("\\");  // \
+
+// 2
+console.log(5 ** 4 - 9);  // 616
+
+// 3
+console.log("I".length - "AM".length + ("IRON".length % "MAN".length));  // 0
+
+// 4
+console.log("CAP"[1]);  // A
+console.log("VISION"[0]);  // V
+console.log("OKOYE"[4]);  // E
+console.log("NICK FURY"[0]);  // N
+console.log("GROOT"[0]);  // G
+console.log("DOCTOR STRANGE"[13]);  // E
+console.log("THOR"[3]);  // R
+console.log("STAR-LORD"[0]);  // S
+
+// 5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);  // 29
